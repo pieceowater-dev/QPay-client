231ba541b87eea8ff525b8e85d8c8ba953861469
vitJR1 <kalugin.vitaliy23@gmail.com>
2024-05-07 15:27:52 +0500

    network connection

--- main.py
+++ main.py
@@ -3,27 +3,37 @@ import gc
 import machine
 import network
 import urequests
+# from socketIO_client import SocketIO, LoggingNamespace
 
+wlan = network.WLAN(network.STA_IF)
 
 def connect_to_wifi(ssid, password):
-    wlan = network.WLAN(network.STA_IF)
-    if not wlan.isconnected():
-        print(f"Connecting to WiFi ({ssid})...")
-        wlan.active(True)
-        print("--------------")
-        wlan.connect(ssid, password)
-        retries = 0
-        while not wlan.isconnected():
-            retries += 1
-            print(f"Attempt: {retries}")
-            if retries > 3:  
-                print("Failed to connect to WiFi.")
-                return False
-        print("Connected to WiFi:", ssid)
-        return True
-    else:
-        print("Already connected to WiFi.")
-        return True
+    print(f"WiFi active ({wlan.active()}) and is con ({wlan.isconnected()})")
+    try:
+        if not wlan.isconnected():
+            print(f"WiFi status ({wlan.status})")
+            print(f"Connecting to WiFi ({ssid})...")
+            wlan.active(True)
+            print(f"connecting to {ssid}, {password}")
+            wlan.connect(ssid, password)
+            print(f"mac: {wlan.config('mac')}")
+
+            retries = 0
+            while not wlan.isconnected():
+                retries += 1
+                print(f"Attempt: {retries}, {wlan.ifconfig()}")
+                if retries > 3:  
+                    print("Failed to connect to WiFi.")
+                    return False
+            print("Connected to WiFi:", ssid)
+            return True
+        else:
+            print("Already connected to WiFi.")
+            return True
+    except Exception as e:
+        wlan.disconnect()
+        print(e)
+        return False
 
 def get_storage_info():
     statvfs = os.statvfs('/')
@@ -50,6 +60,18 @@ def fetch_data(url):
         print("Error fetching data:", e)
         return None
 
+def on_bbb_response(*args):
+        print('on_bbb_response', args)
+
+# def socketIo():
+#     socketIO = SocketIO(
+#         'localhost', 8000,
+#         params={"deviceId": "customClientId"},
+#         headers={'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzE0NzMzMjgyLCJleHAiOjE3MTQ3MzY4ODJ9.FwTsvSoxNFyJkGTza1Sgdx5mhetgqjWmF9Ts9C_6C0E'}
+#     )
+#     socketIO.emit('subscribe', {"deviceId": "customClientId"}, on_bbb_response)
+#     socketIO.wait_for_callbacks(seconds=1)
+
 def init():
     total_storage, free_storage = get_storage_info()
     ram_size = get_ram_info()
@@ -65,18 +87,18 @@ def init():
 
 
     # Connect to Wi-Fi
-    ssid = 'Safe Level of Radiation'
-    password = 'Idontwanttosettheworldonfire'
-    # ssid = 'mid - iphone'
-    # password = 'mid12345'
+    #ssid = 'OnePlus vIt'
+    #password = 'goodgames'
+    ssid = 'mid - iphone'
+    password = 'mid12345'
     if not connect_to_wifi(ssid, password):
         return
 
-    # # Fetch data from JSON file
-    # url = 'http://pieceowater.github.io/resume/src/data.json'
-    # data = fetch_data(url)
-    # if data:
-    #     print("\nData from JSON file:")
-    #     print(data)
+    # Fetch data from JSON file
+    url = 'http://pieceowater.github.io/resume/src/data.json'
+    data = fetch_data(url)
+    if data:
+        print("\nData from JSON file:")
+        print(data)
 
 init()
