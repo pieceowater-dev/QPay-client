a51dea4f9e5f98d92caa837972502920dd26e9c1
pieceowater <pieceowater@gmail.com>
2024-05-08 23:07:19 +0500

    some changes

--- boot.py
+++ boot.py
@@ -1,3 +1,76 @@
 # boot.py -- run on boot-up
+import network
 
-print("boot!")
+
+WIFI_NAME = "Safe Level of Radiation"
+WIFI_PASS = "Idontwanttosettheworldonfire"
+
+WLAN_INSTANCE = network.WLAN(network.STA_IF)
+
+def connect_to_wifi(ssid, password):
+    print(f"WiFi active ({WLAN_INSTANCE.active()}) and is con ({WLAN_INSTANCE.isconnected()})")
+    try:
+        WLAN_INSTANCE.connect()
+        if not WLAN_INSTANCE.isconnected():
+            print(f"WiFi status ({WLAN_INSTANCE.status})")
+            print(f"Connecting to WiFi ({ssid})...")
+            WLAN_INSTANCE.active(True)
+            print(f"connecting to {ssid}, {password}")
+            WLAN_INSTANCE.connect(ssid, password)
+            print(f"mac: {WLAN_INSTANCE.config('mac')}")
+
+            retries = 0
+            while not WLAN_INSTANCE.isconnected():
+                retries += 1
+                print(f"Attempt: {retries}, {WLAN_INSTANCE.ifconfig()}")
+                if retries > 3:  
+                    print("Failed to connect to WiFi.")
+                    return False
+            print("Connected to WiFi:", ssid)
+            return True
+        else:
+            print("Already connected to WiFi.")
+            return True
+    except Exception as e:
+        WLAN_INSTANCE.disconnect()
+        print(e)
+        return False
+
+
+def get_storage_info():
+    statvfs = os.statvfs('/')
+    total_storage = statvfs[0] * statvfs[2] / (1024 * 1024) 
+    free_storage = statvfs[0] * statvfs[3] / (1024 * 1024)  
+    return total_storage, free_storage
+
+def get_ram_info():
+    ram_size = gc.mem_free() / (1024 * 1024)
+    return ram_size
+
+
+
+def init():
+    total_storage, free_storage = get_storage_info()
+    ram_size = get_ram_info()
+
+    print(">>>>>>>>>>>>>>>>>>>>>>")
+    print("RAM Size: {:.2f} MB\n".format(ram_size))
+    print("Total Storage: {:.2f} MB".format(total_storage))
+    print("Free Storage: {:.2f} MB".format(free_storage))
+    print(">>>>>>>>>>>>>>>>>>>>>>")
+
+
+    #ssid = 'OnePlus vIt'
+    #password = 'goodgames'
+    # ssid = 'mid - iphone'
+    # password = 'mid12345'
+    ssid = WIFI_NAME
+    password = WIFI_PASS
+
+    if not connect_to_wifi(ssid, password):
+        return
+
+    print("BOOTED UP!")
+
+
+init()
--- main.py
+++ main.py
@@ -1,49 +1,12 @@
-import os
-import gc
-import machine
-import network
+# import os
+# import gc
+# import machine
+# import network
+
 import urequests
 # from socketIO_client import SocketIO, LoggingNamespace
 
-wlan = network.WLAN(network.STA_IF)
 
-def connect_to_wifi(ssid, password):
-    print(f"WiFi active ({wlan.active()}) and is con ({wlan.isconnected()})")
-    try:
-        if not wlan.isconnected():
-            print(f"WiFi status ({wlan.status})")
-            print(f"Connecting to WiFi ({ssid})...")
-            wlan.active(True)
-            print(f"connecting to {ssid}, {password}")
-            wlan.connect(ssid, password)
-            print(f"mac: {wlan.config('mac')}")
-
-            retries = 0
-            while not wlan.isconnected():
-                retries += 1
-                print(f"Attempt: {retries}, {wlan.ifconfig()}")
-                if retries > 3:  
-                    print("Failed to connect to WiFi.")
-                    return False
-            print("Connected to WiFi:", ssid)
-            return True
-        else:
-            print("Already connected to WiFi.")
-            return True
-    except Exception as e:
-        wlan.disconnect()
-        print(e)
-        return False
-
-def get_storage_info():
-    statvfs = os.statvfs('/')
-    total_storage = statvfs[0] * statvfs[2] / (1024 * 1024) 
-    free_storage = statvfs[0] * statvfs[3] / (1024 * 1024)  
-    return total_storage, free_storage
-
-def get_ram_info():
-    ram_size = gc.mem_free() / (1024 * 1024)
-    return ram_size
 
 def fetch_data(url):
     try:
@@ -60,8 +23,8 @@ def fetch_data(url):
         print("Error fetching data:", e)
         return None
 
-def on_bbb_response(*args):
-        print('on_bbb_response', args)
+# def on_bbb_response(*args):
+        # print('on_bbb_response', args)
 
 # def socketIo():
 #     socketIO = SocketIO(
@@ -73,29 +36,7 @@ def on_bbb_response(*args):
 #     socketIO.wait_for_callbacks(seconds=1)
 
 def init():
-    total_storage, free_storage = get_storage_info()
-    ram_size = get_ram_info()
-
-    print("Device Storage:")
-    print("Total Storage: {:.2f} MB".format(total_storage))
-    print("Free Storage: {:.2f} MB".format(free_storage))
-
-    print("\nRAM:")
-    print("RAM Size: {:.2f} MB".format(ram_size))
-    print("")
-
-
-
-    # Connect to Wi-Fi
-    #ssid = 'OnePlus vIt'
-    #password = 'goodgames'
-    ssid = 'mid - iphone'
-    password = 'mid12345'
-    if not connect_to_wifi(ssid, password):
-        return
-
-    # Fetch data from JSON file
-    url = 'http://pieceowater.github.io/resume/src/data.json'
+    url = 'https://pieceowater.github.io/resume/src/data.json'
     data = fetch_data(url)
     if data:
         print("\nData from JSON file:")
